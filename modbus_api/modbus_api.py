96a61fbd8ab5afd4c22472c10884005d5776ed88
LiuWei <183074632@qq.com>
2025-06-15 13:03:24 +0800

    feature: add save log handler

--- modbus_api/modbus_api.py
+++ modbus_api/modbus_api.py
@@ -1,6 +1,9 @@
 """封装Modbus读写方法."""
 import logging
+import os
+import pathlib
 import struct
+from logging.handlers import TimedRotatingFileHandler
 from typing import Union, List
 
 from modbus_tk import modbus_tcp
@@ -12,14 +15,70 @@ from modbus_api.exception import PLCConnectError, PLCReadError, PLCWriteError
 
 class ModbusApi:
     """ModbusApi class."""
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
 
-    def __init__(self, plc_ip, port=502):
+
+    def __init__(self, plc_ip: str, port: int = 502, plc_name: str = ""):
+        """ModbusApi 构造方法.
+
+        Args:
+            plc_ip: plc ip address.
+            port: port number.
+            plc_name: plc name.
+        """
+        logging.basicConfig(level=logging.INFO, encoding="UTF-8", format=self.LOG_FORMAT)
+
         self.plc_ip = plc_ip
         self.port = port
         self.logger = logging.getLogger(__name__)
+        self.plc_name = plc_name if plc_name else self.plc_ip
 
         self.client = modbus_tcp.TcpMaster(host=plc_ip, port=port)
         self._connection_state = False
+
+        self._file_handler = None  # 保存日志的处理器
+        self._initial_log_config()
+
+    def _initial_log_config(self) -> None:
+        """日志配置."""
+        self._create_log_dir()
+        self.logger.addHandler(self.file_handler)  # handler_passive 日志保存到统一文件
+
+    @staticmethod
+    def _create_log_dir():
+        """判断log目录是否存在, 不存在就创建."""
+        log_dir = pathlib.Path(f"{os.getcwd()}/log")
+        if not log_dir.exists():
+            os.mkdir(log_dir)
+
+    @property
+    def file_handler(self) -> TimedRotatingFileHandler:
+        """设置保存日志的处理器, 每隔 24h 自动生成一个日志文件.
+
+        Returns:
+            TimedRotatingFileHandler: 返回 TimedRotatingFileHandler 日志处理器.
+        """
+        if self._file_handler is None:
+            self._file_handler = TimedRotatingFileHandler(
+                f"{os.getcwd()}/log/plc_{self.plc_name}.log",
+                when="D", interval=1, backupCount=10, encoding="UTF-8"
+            )
+            self._file_handler.namer = self._custom_log_name
+            self._file_handler.setFormatter(logging.Formatter(self.LOG_FORMAT))
+        return self._file_handler
+
+    def _custom_log_name(self, log_path: str):
+        """自定义新生成的日志名称.
+
+        Args:
+            log_path: 原始的日志文件路径.
+
+        Returns:
+            str: 新生成的自定义日志文件路径.
+        """
+        _, suffix, date_str = log_path.split(".")
+        new_log_path = f"{os.getcwd()}/log/plc_{self.plc_name}_{date_str}.{suffix}"
+        return new_log_path
 
     @property
     def ip(self):
@@ -77,7 +136,7 @@ class ModbusApi:
             )
             value = (registers[0] & (1 << bit_index)) != 0
             if save_log:
-                self.logger.info("Read boolean value %s from address %d, bit index %d", value, address, bit_index)
+                self.logger.info("读取 bool 地址 %s 值为: %s, bit 位是: %s", address, value, bit_index)
             return value
         except Exception as e:
             self.logger.error("读取保持寄存器时出错: %s", str(e))
@@ -102,7 +161,7 @@ class ModbusApi:
             else:
                 int_value = registers
             if save_log:
-                self.logger.info("Read int value from %s to %s: %s", address, address + count - 1, int_value)
+                self.logger.info("读取 int 地址 %s 值为: %s", address, int_value)
             return int_value
         except Exception as e:
             self.logger.error("读取输入寄存器时出错: %s", str(e))
@@ -124,7 +183,7 @@ class ModbusApi:
             byte_data = b"".join(struct.pack(">H", result) for result in results)
             value_str = byte_data.decode("UTF-8").strip("\x00")
             if save_log:
-                self.logger.info("Read string value from %s to %s: %s", address, address + count - 1, value_str)
+                self.logger.info("读取 str 地址 %s 值为: %s, 长度为: %s", address, value_str, count)
             return value_str
         except Exception as e:
             self.logger.error("读取输入寄存器时出错: %s", str(e))
@@ -151,7 +210,7 @@ class ModbusApi:
             self.client.execute(
                 slave=1, function_code=cst.WRITE_SINGLE_REGISTER, starting_address=address, output_value=new_value)
             if save_log:
-                self.logger.info("Wrote boolean value %s to address %d, bit index %d", value, address, bit_index)
+                self.logger.info("向 bool 地址 %s 写入值 %s, bit 位是: %s", address, value, bit_index)
         except Exception as e:
             self.logger.error("写入保持寄存器时出错: %s", str(e))
             raise PLCWriteError(f"写入保持寄存器时出错: {e}") from e
@@ -171,7 +230,7 @@ class ModbusApi:
                 slave=1, function_code=cst.WRITE_MULTIPLE_REGISTERS, starting_address=address, output_value=value
             )
             if save_log:
-                self.logger.info("Wrote %s to address %d", value, address)
+                self.logger.info("向 int 地址 %s 写入值 %s", address, value)
         except Exception as e:
             self.logger.error("写入输入寄存器时出错: %s", str(e))
             raise PLCWriteError(f"写入输入寄存器时出错: {e}") from e
